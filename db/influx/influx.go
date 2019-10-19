9989257aa99296bd9d1e2480d313fc16fc47b638
Wojciech Szymski <wojciechszymski@gmail.com>
2019-10-19 17:09:24 +0200

    Add support for InfluxDB.
    Generalize the concept of data output.

---
+++ db/influx/influx.go
@@ -0,0 +1,143 @@
+package influx
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	_ "github.com/influxdata/influxdb1-client"
+	influxdb "github.com/influxdata/influxdb1-client/v2"
+	"github.com/s5i/ruuvi2db/data"
+)
+
+// NewDB returns an object that can be used to connect and push to Influx DB.
+func NewDB() *influxDB {
+	return &influxDB{
+		dp: make(chan []data.Point),
+	}
+}
+
+// WithUsername causes Run to use a given username to connect to DB.
+func WithUsername(u string) runOption {
+	return func(idb *influxDB) error {
+		idb.username = u
+		return nil
+	}
+}
+
+// WithPassword causes Run to use a given password to connect to DB.
+func WithPassword(p string) runOption {
+	return func(idb *influxDB) error {
+		idb.password = p
+		return nil
+	}
+}
+
+// WithPrecision causes Run to use a given precision when pushing to DB (default is seconds).
+func WithPrecision(p string) runOption {
+	return func(idb *influxDB) error {
+		idb.precision = p
+		return nil
+	}
+}
+
+// WithRetentionPolicy causes Run to use a given retention policy when pushing to DB.
+func WithRetentionPolicy(rp string) runOption {
+	return func(idb *influxDB) error {
+		idb.retentionPolicy = rp
+		return nil
+	}
+}
+
+// WithWriteConsistency causes Run to use a given write consistency when pushing to DB.
+func WithWriteConsistency(wc string) runOption {
+	return func(idb *influxDB) error {
+		idb.writeConsistency = wc
+		return nil
+	}
+}
+
+// Run starts a connection to DB and handles Push calls.
+func (idb *influxDB) Run(ctx context.Context, connection, database, table string, opts ...runOption) error {
+	idb.connection = connection
+	idb.database = database
+	idb.table = table
+
+	for _, opt := range opts {
+		if err := opt(idb); err != nil {
+			return fmt.Errorf("run option failed: %v", err)
+		}
+	}
+
+	c, err := influxdb.NewHTTPClient(influxdb.HTTPConfig{
+		Addr:     idb.connection,
+		Username: idb.username,
+		Password: idb.password,
+	})
+	if err != nil {
+		return fmt.Errorf("influxdb.NewHTTPClient failed: %v", err)
+	}
+	defer c.Close()
+
+	for {
+		select {
+		case points := <-idb.dp:
+			bp, err := influxdb.NewBatchPoints(influxdb.BatchPointsConfig{
+				Database:         idb.database,
+				Precision:        idb.precision,
+				RetentionPolicy:  idb.retentionPolicy,
+				WriteConsistency: idb.writeConsistency,
+			})
+			if err != nil {
+				log.Printf("influxdb.NewBatchPoints failed: %v", err)
+				continue
+			}
+			for _, p := range points {
+				pt, err := influxdb.NewPoint(
+					idb.table,
+					map[string]string{
+						"address": p.Address,
+					},
+					map[string]interface{}{
+						"temperature": p.Temperature,
+						"humidity":    p.Humidity,
+						"pressure":    p.Pressure,
+						"battery":     p.Battery,
+					},
+					p.Timestamp,
+				)
+				if err != nil {
+					log.Printf("influxdb.NewPoint failed: %v", err)
+					continue
+				}
+				bp.AddPoint(pt)
+
+				if err := c.Write(bp); err != nil {
+					log.Printf("c.Write failed: %v", err)
+				}
+			}
+
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
+
+// Push attempts to send data to DB.
+func (ib *influxDB) Push(points []data.Point) {
+	ib.dp <- points
+}
+
+type influxDB struct {
+	dp               chan []data.Point
+	connection       string
+	database         string
+	table            string
+	username         string
+	password         string
+	precision        string
+	retentionPolicy  string
+	writeConsistency string
+}
+
+type runOption func(*influxDB) error
--- main.go
+++ main.go
@@ -12,13 +12,25 @@ import (
 	"github.com/paypal/gatt"
 	"github.com/s5i/ruuvi2db/bluetooth"
 	"github.com/s5i/ruuvi2db/data"
+	"github.com/s5i/ruuvi2db/db/influx"
 	"github.com/s5i/ruuvi2db/protocol"
 )
 
 var (
 	hciID       = flag.Int("hci_device_id", -1, "HCI device to use. -1 probes everything.")
 	pointPeriod = flag.Duration("point_period", 5*time.Second, "How often to report a point.")
+
 	logToStdout = flag.Bool("log_to_stdout", false, "Whether to log readings to STDOUT.")
+
+	logToInflux            = flag.Bool("log_to_influx", false, "Whether to log readings to InfluxDB.")
+	influxConnection       = flag.String("influx_connection", "http://localhost:8086", "InfluxDB connection string.")
+	influxDatabase         = flag.String("influx_database", "ruuvi", "InfluxDB database.")
+	influxTable            = flag.String("influx_table", "ruuvi", "InfluxDB table.")
+	influxUsername         = flag.String("influx_username", "", "Username used to connect to InfluxDB.")
+	influxPassword         = flag.String("influx_password", "", "Password used to connect to InfluxDB.")
+	influxPrecision        = flag.String("influx_precision", "s", "Precision specified when pushing data to InfluxDB.")
+	influxRetentionPolicy  = flag.String("influx_retention_policy", "", "Retention policy specified when pushing data to InfluxDB.")
+	influxWriteConsistency = flag.String("influx_write_consistency", "", "Write consistency specified when pushing data to InfluxDB.")
 )
 
 func main() {
@@ -39,20 +51,50 @@ func main() {
 		}
 	}()
 
+	type output interface {
+		Push(points []data.Point)
+	}
+	outputs := []output{}
+
+	if *logToStdout {
+		outputs = append(outputs, &stdout{})
+	}
+
+	if *logToInflux {
+		db := influx.NewDB()
+		go func() {
+			if err := db.Run(ctx, *influxConnection, *influxDatabase, *influxTable,
+				influx.WithUsername(*influxUsername),
+				influx.WithPassword(*influxPassword),
+				influx.WithPrecision(*influxPrecision),
+				influx.WithRetentionPolicy(*influxRetentionPolicy),
+				influx.WithWriteConsistency(*influxWriteConsistency)); err != nil {
+				log.Fatalf("influx: db.Run failed: %v", err)
+			}
+		}()
+		outputs = append(outputs, db)
+	}
+
 	go func() {
 		for now := range time.Tick(*pointPeriod) {
 			pts := buf.PullAll(now)
 			sort.Slice(pts, func(i, j int) bool {
 				return strings.Compare(pts[i].Address, pts[j].Address) < 0
 			})
-			if *logToStdout {
-				fmt.Println("---")
-				for _, p := range pts {
-					fmt.Println(p)
-				}
+			for _, out := range outputs {
+				out.Push(pts)
 			}
 		}
 	}()
 
 	select {}
+}
+
+type stdout struct{}
+
+func (s *stdout) Push(points []data.Point) {
+	fmt.Println("---")
+	for _, p := range points {
+		fmt.Println(p)
+	}
 }
