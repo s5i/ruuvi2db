b8d8559ddfad648f5893cc05b2f7ed6104ecb946
Wojciech Szymski <wojciechszymski@gmail.com>
2019-10-19 15:48:13 +0200

    Add a cyclical buffer structure to hold data points.

---
+++ data/buffer.go
@@ -0,0 +1,82 @@
+package data
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type buffer struct {
+	mu       sync.RWMutex
+	readings map[string][]Point
+	nextIdx  map[string]int
+	size     int
+}
+
+// NewBuffer creates a buffer for data point readings.
+// Size specifies how many points should be kept per device address, automatically adjusted to be at least 2.
+func NewBuffer(size int) *buffer {
+	if size < 2 {
+		size = 2
+	}
+	return &buffer{
+		readings: map[string][]Point{},
+		nextIdx:  map[string]int{},
+		size:     size,
+	}
+}
+
+// Push adds a data point for a given device address.
+func (b *buffer) Push(p Point) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	if b.readings[p.Address] == nil {
+		b.readings[p.Address] = make([]Point, b.size, b.size)
+		for i := range b.readings[p.Address] {
+			b.readings[p.Address][i] = p
+		}
+	}
+
+	b.readings[p.Address][b.nextIdx[p.Address]] = p
+	b.nextIdx[p.Address]++
+	b.nextIdx[p.Address] %= b.size
+}
+
+// Pull returns a data point for a given device address.
+func (b *buffer) Pull(addr string, timestamp time.Time) (Point, error) {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+
+	if b.readings[addr] == nil {
+		return Point{}, fmt.Errorf("no readings for device %s", addr)
+	}
+
+	p, err := LinearExtrapolate(b.readings[addr], timestamp)
+	if err != nil {
+		return Point{}, fmt.Errorf("failed to extrapolate readings for device %s: %v", addr, err)
+	}
+
+	return p, nil
+}
+
+// PullAll returns data points for all known devices.
+func (b *buffer) PullAll(timestamp time.Time) []Point {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+
+	ret := []Point{}
+	for addr := range b.readings {
+		if b.readings[addr] == nil {
+			continue
+		}
+
+		p, err := LinearExtrapolate(b.readings[addr], timestamp)
+		if err != nil {
+			continue
+		}
+
+		ret = append(ret, p)
+	}
+	return ret
+}
