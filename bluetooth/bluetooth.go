d4a92619845f9d4de29e9cd73f52f681ac8b9ef8
Wojciech Szymski <wojciechszymski@gmail.com>
2019-10-19 13:56:52 +0200

    Add a Bluetooth scanning function.
    Executes a callback whenever a peripheral device is discovered.

---
+++ bluetooth/bluetooth.go
@@ -0,0 +1,40 @@
+package bluetooth
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/paypal/gatt"
+)
+
+func Run(ctx context.Context, hciID int, callback func(p gatt.Peripheral, a *gatt.Advertisement, rssi int)) error {
+	d, err := gatt.NewDevice(gatt.LnxMaxConnections(1), gatt.LnxDeviceID(hciID, true))
+	if err != nil {
+		return fmt.Errorf("gatt.NewDevice failed: %v", err)
+	}
+
+	d.Handle(gatt.PeripheralDiscovered(callback))
+
+	powerOn := make(chan gatt.Device)
+	powerOff := make(chan gatt.Device)
+
+	d.Init(func(d gatt.Device, s gatt.State) {
+		if s == gatt.StatePoweredOn {
+			powerOn <- d
+		}
+		if s == gatt.StatePoweredOff {
+			powerOff <- d
+		}
+	})
+
+	for {
+		select {
+		case d := <-powerOn:
+			d.Scan(nil, true)
+		case d := <-powerOff:
+			d.StopScanning()
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/paypal/gatt"
+	"github.com/s5i/ruuvi2db/bluetooth"
+)
+
+var (
+	hciID = flag.Int("hci_device_id", -1, "HCI device to use. -1 probes everything.")
+)
+
+func main() {
+	flag.Parse()
+	ctx := context.Background()
+
+	go func() {
+		if err := bluetooth.Run(ctx, *hciID, func(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
+			// TODO(s5i): implement handler; the data lives in a.ManufacturerData and p.ID()
+		}); err != nil {
+			log.Fatalf("bluetooth.Run failed: %v", err)
+		}
+	}()
+
+	select {}
+}
