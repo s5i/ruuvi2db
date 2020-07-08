973a89f39a7e67f341642adc2b37610fac775454
Wojciech Szymski <wojciechszymski@gmail.com>
2020-07-08 18:26:48 +0200

    Add an interface to facilitate getting data out of DBs.

--- db/interface.go
+++ db/interface.go
@@ -1,7 +1,15 @@
 package db
 
-import "github.com/s5i/ruuvi2db/data"
+import (
+	"time"
+
+	"github.com/s5i/ruuvi2db/data"
+)
 
 type Interface interface {
 	Push(points []data.Point)
+}
+
+type Source interface {
+	Get(startTime, endTime time.Time) map[string][]data.Point
 }
