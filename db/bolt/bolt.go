44cc1e0cbe58e3ba4d9d2697b82dfdaadbe316ba
Wojciech Szymski <wojciechszymski@gmail.com>
2020-07-08 18:29:11 +0200

    Implement db.Source functionality for Bolt.

--- db/bolt/bolt.go
+++ db/bolt/bolt.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/binary"
 	"fmt"
+	"log"
+	"sort"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -12,10 +14,11 @@ import (
 	"github.com/s5i/ruuvi2db/db"
 )
 
-// NewDB returns an object that can be used to connect and push to SQLite DB.
+// NewDB returns an object that can be used to connect and push to Bolt DB.
 func NewDB() *boltDB {
 	return &boltDB{
-		dp:         make(chan []data.Point, 1),
+		pushCh:     make(chan []data.Point, 1),
+		getCh:      make(chan getReq),
 		bucketSize: 24 * time.Hour,
 		retention:  7 * 24 * time.Hour,
 	}
@@ -54,7 +57,7 @@ func (bdb *boltDB) Run(ctx context.Context, path string, opts ...runOption) erro
 
 	for {
 		select {
-		case points := <-bdb.dp:
+		case points := <-bdb.pushCh:
 			for _, p := range points {
 				db.Update(func(tx *bolt.Tx) error {
 					b, err := tx.CreateBucketIfNotExists(bucketFromTimestamp(p.Timestamp, bdb.bucketSize))
@@ -76,6 +79,26 @@ func (bdb *boltDB) Run(ctx context.Context, path string, opts ...runOption) erro
 					return b.Put(k, v)
 				})
 			}
+		case getReq := <-bdb.getCh:
+			points := []data.Point{}
+			db.View(func(tx *bolt.Tx) error {
+				return tx.ForEach(func(name []byte, b *bolt.Bucket) error {
+					bStart := timestampFromBucket(name)
+					if bStart.Before(getReq.startTime.Add(-bdb.bucketSize)) || getReq.endTime.Before(bStart) {
+						return nil
+					}
+					return b.ForEach(func(k, v []byte) error {
+						p, err := data.DecodePoint(v)
+						if err != nil {
+							log.Printf("badly encoded point %v", v)
+							return nil
+						}
+						points = append(points, p)
+						return nil
+					})
+				})
+			})
+			getReq.ret <- points
 
 		case <-ctx.Done():
 			return nil
@@ -87,17 +110,36 @@ func (bdb *boltDB) Run(ctx context.Context, path string, opts ...runOption) erro
 
 // Push attempts to send data to DB.
 func (bdb *boltDB) Push(points []data.Point) {
-	bdb.dp <- points
+	bdb.pushCh <- points
+}
+
+func (bdb *boltDB) Get(startTime, endTime time.Time) map[string][]data.Point {
+	req := getReq{
+		startTime: startTime,
+		endTime:   endTime,
+		ret:       make(chan []data.Point),
+	}
+	bdb.getCh <- req
+	return sortAndSplit(<-req.ret)
 }
 
 type boltDB struct {
-	dp         chan []data.Point
+	pushCh chan []data.Point
+	getCh  chan getReq
+
 	dbPath     string
 	bucketSize time.Duration
 	retention  time.Duration
 }
 
+type getReq struct {
+	startTime time.Time
+	endTime   time.Time
+	ret       chan []data.Point
+}
+
 var _ db.Interface = (*boltDB)(nil)
+var _ db.Source = (*boltDB)(nil)
 
 func bucketFromTimestamp(timestamp time.Time, truncate time.Duration) []byte {
 	ts := uint64(timestamp.Truncate(truncate).UnixNano())
@@ -106,8 +148,29 @@ func bucketFromTimestamp(timestamp time.Time, truncate time.Duration) []byte {
 	return b
 }
 
+func timestampFromBucket(b []byte) time.Time {
+	if len(b) != 8 {
+		return time.Time{}
+	}
+	return time.Unix(0, int64(binary.BigEndian.Uint64(b)))
+}
+
 func keyFromSeq(seq uint64) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, seq)
 	return b
+}
+
+func sortAndSplit(points []data.Point) map[string][]data.Point {
+	ret := map[string][]data.Point{}
+
+	for _, p := range points {
+		ret[p.Address] = append(ret[p.Address], p)
+	}
+
+	for a := range ret {
+		sort.Slice(ret[a], func(i, j int) bool { return ret[a][i].Timestamp.Before(ret[a][j].Timestamp) })
+	}
+
+	return ret
 }
